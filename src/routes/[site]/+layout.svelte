87ba788b294c32ae257bab302b37716293f94980
Mateo Morris <accounts@mateomorris.com>
2023-06-16 17:04:09 -0400

    update primo

--- src/routes/[site]/+layout.svelte
+++ src/routes/[site]/+layout.svelte
@@ -1,34 +1,7 @@
 <script>
-	import { onDestroy } from 'svelte'
-	import { browser } from '$app/environment'
-	import Primo, { stores } from '@primocms/builder'
-	// import modal from 'primo-editor/stores/app/modal'
-	import { page } from '$app/stores'
+	import Primo from '@primocms/builder'
 
 	export let data
-
-	let showing_sidebar = false
-
-	let leftPaneSize = browser ? (showing_sidebar ? window.innerWidth / 5 + `px` : '0px') : '200px'
-	let rightPaneSize = browser
-		? showing_sidebar
-			? (window.innerWidth / 5) * 5 + 'px'
-			: 'auto'
-		: 'auto'
-
-	$: if (parseInt(leftPaneSize) < 100) {
-		leftPaneSize = '20px'
-		rightPaneSize = '100%'
-		showing_sidebar = false
-	} else if (parseInt(leftPaneSize) >= 100 && !showing_sidebar) {
-		reset()
-	}
-
-	function reset() {
-		leftPaneSize = browser ? window.innerWidth / 5 + 'px' : '0px'
-		rightPaneSize = browser ? (window.innerWidth / 5) * 5 + 'px' : '0px'
-		showing_sidebar = true
-	}
 </script>
 
 <Primo {data}>
